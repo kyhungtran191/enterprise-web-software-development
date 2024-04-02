c1b7b9d120526a77d689bd9b5d2cc69211d423a4
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-04-02 20:10:20 +0700

    Fix: remove field reset

--- server/Server.Application/Features/Identity/Users/Commands/ResetPassword/ResetPasswordCommand.cs
+++ server/Server.Application/Features/Identity/Users/Commands/ResetPassword/ResetPasswordCommand.cs
@@ -8,6 +8,5 @@ namespace Server.Application.Features.Identity.Users.Commands.ResetPassword
     {
         public string Token { get; set; }
         public string Password { get; set; }
-        public string Email { get; set; }
     }
 }
