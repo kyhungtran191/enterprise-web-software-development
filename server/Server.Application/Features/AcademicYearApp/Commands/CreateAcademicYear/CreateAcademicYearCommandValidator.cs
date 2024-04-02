94acf85815ee1ba8f74dc2afff21e0fd28e844bb
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-04-02 15:38:08 +0700

    Feat(academic) : add year name validation

--- server/Server.Application/Features/AcademicYearApp/Commands/CreateAcademicYear/CreateAcademicYearCommandValidator.cs
+++ server/Server.Application/Features/AcademicYearApp/Commands/CreateAcademicYear/CreateAcademicYearCommandValidator.cs
@@ -8,7 +8,26 @@ namespace Server.Application.Features.AcademicYearApp.Commands.CreateAcademicYea
         {
             RuleFor(request => request.Name)
                 .NotEmpty()
-                .WithMessage("Academic year name is required.");
+                .WithMessage("Academic year name is required.")
+                .Matches(@"^\d{4}-\d{4}$")
+                .WithMessage("Academic year name must be in the format 'XXXX-YYYY'.")
+                .Must(BeConsecutiveYears)
+                .WithMessage("The years must be consecutive.");
+        }
+        private bool BeConsecutiveYears(string name)
+        {
+            var years = name.Split('-');
+            if (years.Length != 2)
+            {
+                return false;
+            }
+
+            if (int.TryParse(years[0], out int startYear) && int.TryParse(years[1], out int endYear))
+            {
+                return endYear - startYear == 1;
+            }
+
+            return false;
         }
     }
 }
