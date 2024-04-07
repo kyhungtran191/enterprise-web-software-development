4f0cc8d0d832dff5ae1df63eaf42206c45f4a50d
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-04-07 23:52:53 +0700

    Fix(comment): fix comment

--- server/Server.Application/Features/Identity/Users/Commands/UpdateProfile/UpdateProfileValidator.cs
+++ server/Server.Application/Features/Identity/Users/Commands/UpdateProfile/UpdateProfileValidator.cs
@@ -6,11 +6,11 @@ namespace Server.Application.Features.Identity.Users.Commands.UpdateProfile
     {
         public UpdateProfileValidator()
         {
-            RuleFor(x => x.Email)
-                .MaximumLength(255)
-                .EmailAddress()
-                .NotEmpty()
-                .WithMessage("Please enter an valid email");
+            //RuleFor(x => x.Email)
+            //    .MaximumLength(255)
+            //    .EmailAddress()
+            //    .NotEmpty()
+            //    .WithMessage("Please enter an valid email");
 
             RuleFor(x => x.FirstName)
                 .MaximumLength(255)
@@ -20,7 +20,7 @@ namespace Server.Application.Features.Identity.Users.Commands.UpdateProfile
                 .MaximumLength(255)
                 .NotEmpty()
                 .WithMessage("Last name is required");
-            RuleFor(x => x.PhoneNumber).NotEmpty().MaximumLength(50).WithMessage("Please enter phone number");
+            //RuleFor(x => x.PhoneNumber).NotEmpty().MaximumLength(50).WithMessage("Please enter phone number");
 
 
         }
