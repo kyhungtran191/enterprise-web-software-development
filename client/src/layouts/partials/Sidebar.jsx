3876ec6080d72c6007203728aa3b769a558a5c2f
justinJSwebdev <trankyhung225@gmail.com>
2024-04-20 08:54:30 +0700

    fix sidebar

--- client/src/layouts/partials/Sidebar.jsx
+++ client/src/layouts/partials/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 import { Link, useLocation } from 'react-router-dom'
+import { ShieldCloseIcon } from 'lucide-react'
 
 export function Sidebar({ className, links }) {
   // Use useLocation hook to get the current location
@@ -9,23 +10,24 @@ export function Sidebar({ className, links }) {
 
   return (
     <>
-      <div className={cn('pb-12 shadow-xl', className)}>
-        <div className='py-4 space-y-4'>
-          <div className='medium:py-2 medium:px-3'>
-            <div className='space-y-1'>
+      <div className={cn('', className)}>
+        <div className='py-4 space-y-4 '>
+          <div className='medium:py-2 medium:px-3 '>
+            <div className='flex flex-wrap items-center gap-1 space-y-1 justify-evenly sm:justify-start sm:flex-col'>
+
               {links.length > 0 &&
                 links.map((link, index) => (
-                  <Link to={link.href} key={index}>
+                  <Link to={link.href} key={index} >
                     {' '}
                     <Button
                       key={index}
                       variant={
                         location.pathname === link.href ? 'secondary' : 'ghost'
                       }
-                      className='justify-center medium:justify-start w-full !px-2 !medium:py-2 !medium:px-4'
+                      className='justify-center medium:justify-start w-full !px-2 !medium:py-2 !medium:px-4 flex flex-col sm:flex-row'
                     >
-                      <link.icon className='w-4 h-4 mr-2' />
-                      <span className='hidden medium:block'>{link.title}</span>
+                      <link.icon className='flex-shrink-0 w-4 h-4 mr-2' />
+                      <span className='block sm:hidden medium:block'>{link.title}</span>
                     </Button>
                   </Link>
                 ))}
