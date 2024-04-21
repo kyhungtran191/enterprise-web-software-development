06665a5e79b377a3c1302bb43e90ac288b3a4deb
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-04-21 20:03:00 +0700

    Feat(background job): set up quartz

---
+++ server/Server.Infrastructure/Jobs/LoggingBackgroundJob.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+namespace Server.Infrastructure.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class LoggingBackgroundJob : IJob
+    {
+        private readonly ILogger<LoggingBackgroundJob> _logger;
+
+        public LoggingBackgroundJob(ILogger<LoggingBackgroundJob> logger)
+        {
+            _logger = logger;
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("{UtcNow}",DateTime.UtcNow);
+            return Task.CompletedTask;
+        }
+    }
+}
