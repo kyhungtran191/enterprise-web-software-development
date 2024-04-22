9b777d26bc739fa77d44ab488e13d46bb09d96cc
minhan1910 <annogo123@gmail.com>
2024-04-22 21:52:39 +0700

    done

--- server/Server.Application/Common/Interfaces/Hubs/Announcement/AnnouncementHub.cs
+++ server/Server.Application/Common/Interfaces/Hubs/Announcement/AnnouncementHub.cs
@@ -1,9 +1,7 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Server.Application.Common.Interfaces.Hubs.Announcement;
 
-[Authorize]
 public class AnnouncementHub : Hub<IAnnouncementClient>
 {
 }
