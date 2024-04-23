4e470083c8dd289f17552d69eb22a1fa4accf082
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-04-23 17:52:54 +0700

    Fix(user, guest): fix create user ,guest

--- server/Server.Contracts/Identity/Users/CreateUserRequest.cs
+++ server/Server.Contracts/Identity/Users/CreateUserRequest.cs
@@ -4,15 +4,15 @@ namespace Server.Contracts.Identity.Users;
 
 public class CreateUserRequest
 {
-    public string FirstName { get; set; } = default!;
-    public string LastName { get; set; } = default!;
+    public string? FirstName { get; set; } = default!;
+    public string? LastName { get; set; } = default!;
     public string UserName { get; set; } = default!;
     public string Email { get; set; } = default!;
-    public string PhoneNumber { get; set; } = default!;
+    //public string PhoneNumber { get; set; } = default!;
     // public string Password { get; set; } = default!;
     public Guid FacultyId { get; set; }
     public Guid RoleId { get; set; } = default!;
-    public DateTime? Dob { get; set; }
+    //public DateTime? Dob { get; set; }
     public IFormFile? Avatar { get; set; }
     public bool IsActive { get; set; }
 }
---
+++ server/Server.Infrastructure/Migrations/20240423103101_changeAppUser.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AppUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AppUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AppUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AppUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
