41fe98265396dbaaab1bdebddfdc04dc091f6d07
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-04-23 16:11:30 +0700

    Feat(guest): create guest of coordinator

---
+++ server/Server.Api/Controllers/CoordinatorApi/UserController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Server.Application.Common.Extensions;
+using Server.Application.Features.Identity.Users.Commands.CreateGuest;
+using Server.Contracts.Identity.Users;
+
+namespace Server.Api.Controllers.CoordinatorApi
+{
+    public class UsersController  : CoordinatorApiController
+    {
+        private readonly IMapper _mapper;
+        public UsersController(ISender _sender,IMapper mapper) : base(_sender)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("create-guest")]
+        public async Task<IActionResult> CreateGuest([FromForm] CreateGuestRequest request)
+        {
+            var command = _mapper.Map<CreateGuestCommand>(request);
+            command.FacultyId = User.GetFacultyId();
+            var result = await _mediatorSender.Send(command);
+            return result.Match(result => Ok(result), errors => Problem(errors));
+        }
+    }
+}
