b4f7a5f2b9e48961f2bf40f1d1cf3631c7c150b3
minhan1910 <annogo123@gmail.com>
2024-04-24 22:28:22 +0700

    done

---
+++ server/Server.Application/Common/Dtos/PrivateChat/NewUserConnectedDto.cs
@@ -0,0 +1,7 @@
+namespace Server.Application.Common.Dtos.PrivateChat;
+
+public class NewUserConnectedDto
+{
+    public string? UserId { get; set; }
+    public string? Username { get; set; }
+}
---
+++ server/Server.Application/Common/Dtos/PrivateChat/NewUserDisconnectedDto.cs
@@ -0,0 +1,7 @@
+namespace Server.Application.Common.Dtos.PrivateChat;
+
+public class NewUserDisconnectedDto
+{
+    public string? UserId { get; set; }
+    public string? Username { get; set; }
+}
